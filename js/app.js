b286705bd5b35a7885104a016c95d43b56182007
Maciej Necka <macieknecka@gmail.com>
2023-09-25 10:30:56 +0200

    17/74

--- js/app.js
+++ js/app.js
@@ -1,1 +1,45 @@
-console.log('Hello world');
+'use strict';
+
+// const btnEl = document.querySelector('.btn');
+// if (btnEl) {
+//   // jeśli został wyszukany
+//   btnEl.addEventListener('click', function () {
+//     // wywołaj tę funkcje, jesli wyszukany przycisk zostanie klikniety
+//     console.log('Button was clicked');
+//     // wyswietl napis w konsoli
+//   });
+// }
+
+// const handleClick = function () {
+//   console.log('button was clicked');
+// };
+// // utworzylem wyrażenie funkcyjne, tj. funkcja została przypisana do zadeklarowanej zmiennej
+
+// const btnEl = document.querySelector('.btn');
+// if (btnEl) {
+//   // jesli został wyszukany element...
+
+//   btnEl.addEventListener('click', handleClick);
+//   // nasłuchuj kliknięcia na ten element, tzn. wywołaj funcje handleClick, jesli wyszukany przycisk zostanie kliknięty
+// }
+
+// const handleClick = function () {
+//   console.log('Button was clicked');
+// };
+
+// const btnEl = document.querySelector('.btn');
+// if (btnEl) {
+//   // jesli został wyszukany element...
+//   btnEl.addEventListener('click', handleClick);
+//   // nasłuchuj kliknięcia na ten element, tzn. wywołaj funkcje anonimowa (bez nazwy), jesli wyszukany przycisk zostanie klikniety
+// }
+
+// const contentEl = document.querySelector('content');
+// // jesli plik js jest podpiety do sekcji <head/>, to zmienna [contentEl] bedzie zawierac [null]
+
+// const init = function () {
+//   const contentEl = document.querySelector('.content');
+//   // jesli wyszukanie nastapi po załadowaniu DOM-u, to element zostanie odnaleziony prawidłowo. Dzięki wykorzystaniu DOMContentLoaded miejsce podpiecia pliku JS nie ma juz znaczenia!
+// };
+
+// document.addEventListener('DOMContentLoaded', init);
